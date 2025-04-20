7e42e61c79d1fd811be603d33ffc739d11980e0d
Zongjian Li <orzfirst@vip.qq.com>
2025-04-19 23:11:10 -0700

    initial website

---
+++ README.md
@@ -0,0 +1,19 @@
+# Text2Avatar: Bringing Diversity from Diffusion Models to Semantic-Guided Face Asset Generation
+
+This is the repository that contains source code for the [Text2Avatar project website](https://ict-vision-graphics-lab.github.io/text2avatar-page/).
+
+If you find Text2Avatar useful for your work please cite:
+```
+@article{cai2023text2avatar,
+  author    = {Cai, Xunxuan and Xiang, Sitao and Li, Zongjian and Chen, Haiwei and Zhao, Yajie},
+  title     = {Bringing Diversity from Diffusion Models to Semantic-Guided Face Asset Generation},
+  journal   = {arXiv preprint},
+  year      = {2025},
+}
+```
+
+## Website License
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+## Acknowledgments
+This website template is based on the [Nerfies project website](https://github.com/nerfies/nerfies.github.io). We thank the authors for making their source code available.
---
+++ static/js/index.js
@@ -0,0 +1,104 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+var INTERP_BASE = "./static/interpolation/stacked";
+var NUM_INTERP_FRAMES = 240;
+
+var interp_images = [];
+function preloadInterpolationImages() {
+  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+    interp_images[i] = new Image();
+    interp_images[i].src = path;
+  }
+}
+
+function setInterpolationImage(i) {
+  var image = interp_images[i];
+  image.ondragstart = function() { return false; };
+  image.oncontextmenu = function() { return false; };
+  $('#interpolation-image-wrapper').empty().append(image);
+}
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    var options = {
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+		// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+    	// bulmaCarousel instance is available as element.bulmaCarousel
+    	element.bulmaCarousel.on('before-show', function(state) {
+    		console.log(state);
+    	});
+    }
+
+    /*var player = document.getElementById('interpolation-video');
+    player.addEventListener('loadedmetadata', function() {
+      $('#interpolation-slider').on('input', function(event) {
+        console.log(this.value, player.duration);
+        player.currentTime = player.duration / 100 * this.value;
+      })
+    }, false);*/
+    preloadInterpolationImages();
+
+    $('#interpolation-slider').on('input', function(event) {
+      setInterpolationImage(this.value);
+    });
+    setInterpolationImage(0);
+    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+    bulmaSlider.attach();
+
+    // Face texture slider functionality
+    const faceSlider = document.getElementById('face-slider');
+    if (faceSlider) {
+        const albedoDisplay = document.getElementById('albedo-display');
+        const displacementDisplay = document.getElementById('displacement-display');
+        const specularDisplay = document.getElementById('specular-display');
+        const renderDisplay = document.getElementById('render-display');
+        const faceNumber = document.getElementById('face-number');
+        
+        // Update all texture images based on selected face
+        function updateFaceTextures() {
+            const faceId = faceSlider.value;
+            
+            // Update texture images
+            albedoDisplay.src = `./static/images/face_${faceId}_albedo.png`;
+            displacementDisplay.src = `./static/images/face_${faceId}_displacement.png`;
+            specularDisplay.src = `./static/images/face_${faceId}_specular.png`;
+            renderDisplay.src = `./static/images/face_${faceId}_render.png`;
+            
+            // Update face indicator
+            faceNumber.textContent = `Face Model ${faceId}/${faceSlider.max}`;
+        }
+        
+        // Face slider event listener
+        faceSlider.addEventListener('input', updateFaceTextures);
+    }
+})
